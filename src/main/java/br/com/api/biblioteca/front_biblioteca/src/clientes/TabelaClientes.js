1d69cd80231fc85b4d60d032b2d257b9eef7807e
rodrigo <rodrigo.norys@gmail.com>
2023-06-03 13:55:42 -0300

    Botões e css 2

--- src/main/java/br/com/api/biblioteca/front_biblioteca/src/clientes/TabelaClientes.js
+++ src/main/java/br/com/api/biblioteca/front_biblioteca/src/clientes/TabelaClientes.js
@@ -1,7 +1,27 @@
-import React from 'react';
-
 export default function TabelaClientes() {
     return (
-        <h1>Tabela dos clientes</h1>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>Telefone</th>
+                    <th>Bairro</th>
+                    <th>Rua</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </tbody>
+        </table>
     );
 }
